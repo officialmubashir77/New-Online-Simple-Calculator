75319c3cdd239b40d3a4954200e437d873569273
officialmubashir77 <officialmubashir77@gmail.com>
2021-06-28 01:03:32 +0500

    Simple-Online-Calculator

---
+++ app.js
@@ -0,0 +1,36 @@
+
+
+
+
+function getValue(num){
+
+var input  = document.getElementById("input");
+
+input.value += num
+
+console.log(num)
+
+
+
+}
+
+
+function performeval(){
+
+
+// alert("Han bhai Chl rha hy");
+
+var input = document.getElementById("input") ;
+
+input.value = eval(input.value);
+
+
+
+
+
+}
+
+
+
+
+
